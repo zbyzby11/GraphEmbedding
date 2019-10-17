3325190626f655b865f5227cb7e959a7ad8e90da
zbyzby11 <2533449732@qq.com>
2019-10-17 15:14:54 +0800

    main

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+主函数，用来运行一些算法
+"""
+import time
+
+from readGraph import CreateGraph
+from deepwalk import DeepWalk
+
+
+def main():
+    graph = CreateGraph()
+    graph.read_edgelist('./data/wiki/Wiki_edgelist.txt')
+    t = time.time()
+    DeepWalk(graph)
+    print(time.time() - t)
+
+
+if __name__ == '__main__':
+    main()
