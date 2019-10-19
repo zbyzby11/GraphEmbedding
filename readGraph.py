468b456a18f155f317e58cf3f6b6a02b7b7cd086
zbyzby11 <2533449732@qq.com>
2019-10-19 11:36:19 +0800

    update read graph

--- readGraph.py
+++ readGraph.py
@@ -6,11 +6,15 @@ import networkx as nx
 
 class CreateGraph(object):
     def __init__(self):
+        """
+        创建一个nx图，G表示创建好的一个图，这个图有一些属性，比如节点个数等等（可以再后续补充属性，如邻接矩阵等）
+        """
         self.G = None
         self.node_size = 0
 
     def read_edgelist(self, filename, is_weight=False):
         """
+        前置函数，必须要首先运行这个函数。
         从一个文件中读取出一个图并且建立,需要判断是否有边的权重,默认权重为False，即为1.0
         :param filename: 读取的格式化的文件
         :param is_weight: bool类型参数，表示是否有边的权重
